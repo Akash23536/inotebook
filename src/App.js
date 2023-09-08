5f497666594e3c7e620d6e38e6a50ed79f83771a
Akash Bhadana <23536akash.2021@gmail.com>
2023-09-08 18:45:26 +0530

    alert remove

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 // import About from './components/About';
 import NoteState from './context/notes/NoteState';
-import Alert from './components/Alert';
+// import Alert from './components/Alert';
 // import Signup from './components/Signup';
 // import Login from './components/Login';
 
@@ -19,7 +19,7 @@ function App() {
       <NoteState>
         <Router>
           <Navbar />
-          <Alert message="This is amazing React course" />
+          {/* <Alert message="This is amazing React course" /> */}
           <div className="container">
           <Routes>
               <Route exact path="/" element={<Home />} />
